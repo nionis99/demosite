d894bcb0a6263573333a4c9488b0da9aebd1ecd8
Ignas Stanionis <nionis0697@gmail.com>
2020-08-19 21:11:49 +0300

    pc responsive header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,14 @@ import Navigation from "../Layout/Navigation";
 import Footer from "../Layout/Footer";
 import Content from "../Layout/Content";
 import Contact from "../Contact/Contact";
+import Header from "../Header/Header";
 
 function App() {
     return (
         <Fragment>
             <Navigation/>
             <Content>
+                <Header/>
                 <Contact/>
             </Content>
             <Footer/>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header-welcome">
+                <div className="header-title">Welcome</div>
+                <div className="header-divider"/>
+                <div className="header-text">
+                    Sed laoreet magna at neque sollic-i-tudin, quis tristique ipsum Sed laoreet magna at neque
+                    sollic-i-tudin, quis tristique ipsum Sed laoreet magna at neque sollic-i-tudin, quis tristique
+                </div>
+                <div className="header-text-button">
+                    <button className="text-button">learn more</button>
+                </div>
+            </div>
+            <div className="header-logo">
+                <div className="logo"/>
+                <div className="header-logo-text">
+                    <div className="title">ui/ux design</div>
+                    <div className="description">
+                        Fusce posuere et males uada est, nec convallis euis mod. Nam el
+                        Fusce posuere et males uada est, nec convallis euis mod
+                        <p>Fusce posuere et males uada est, nec convallis euis mod. Nam el</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
