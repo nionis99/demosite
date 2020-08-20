11de1ad642f9714e83bd8a26378644a854984aa8
Ignas Stanionis <nionis0697@gmail.com>
2020-08-20 14:48:21 +0300

    unused import, loader spinner centered in div

--- src/components/Pages/Service.js
+++ src/components/Pages/Service.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Layout from "../Layout/Layout";
-import ContactForm from "./Home/ContactForm";
 
 function Service() {
     return <Layout>
-       Service
+        Service
     </Layout>;
 }
 
