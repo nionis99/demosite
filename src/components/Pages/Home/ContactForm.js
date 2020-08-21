436d478af57d0c8d8a7cca797fa49e0fac174298
Ignas Stanionis <nionis0697@gmail.com>
2020-08-21 14:29:41 +0300

    State hooks for contact form.

--- src/components/Pages/Home/ContactForm.js
+++ src/components/Pages/Home/ContactForm.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
 function ContactForm() {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [message, setMessage] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post("http://test.test/send", {name, email, message})
+            .then(response => console.log(response)).catch(e => alert(e.message));
+    }
+
     return (
         <div className="contact">
             <div className="contact-header">
@@ -17,13 +29,18 @@ function ContactForm() {
                         ultrices euismod. Aliquam mollis urna Cras feugiat tortor vitae ultrices euismod. </p>
                 </div>
                 <div className="contact-form">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name"/>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email"/>
-                    <label htmlFor="subject">Message</label>
-                    <textarea id="message" name="message" wrap={"off"} rows={4}/>
-                    <input type="submit" onClick={() => alert("Not available")} value="Send"/>
+                    <form onSubmit={e => handleSubmit(e)}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" value={name}
+                               onChange={e => setName(e.target.value)}/>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" value={email}
+                               onChange={e => setEmail(e.target.value)}/>
+                        <label htmlFor="subject">Message</label>
+                        <textarea id="message" name="message" wrap={"off"} rows={4} value={message}
+                                  onChange={e => setMessage(e.target.value)}/>
+                        <input type="submit" value="Send"/>
+                    </form>
                 </div>
             </div>
         </div>
